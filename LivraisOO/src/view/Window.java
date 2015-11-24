5efea126aedeb8c244ea0ee4bf0b6c2c16bc4e62
Razvan <stancioiu.razvan@gmail.com>
2015-11-24 21:58:54 +0100

    Solved merge problems

--- LivraisOO/src/view/Window.java
+++ LivraisOO/src/view/Window.java
@@ -4,19 +4,9 @@ import javax.swing.BorderFactory;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
-<<<<<<< HEAD:LivraisOO/src/view/Window.java
 import model.Plan;
 
-public class Window extends JFrame {
-=======
-import model.Map;
-
 public class Window extends JFrame{
->>>>>>> 9a535b2919dfa1f665df945f77774da1c8c10f49:Code java/LivraisOO/src/view/Window.java
-
-	/**
-	 * 
-	 */
 	private static final long serialVersionUID = 1L;
 	private GraphicView graphicView;
 	private LabelView textualView;
@@ -35,7 +25,6 @@ public class Window extends JFrame{
 		textualView.StepOne();
 		graphicView.StepOne();
 	}
-<<<<<<< HEAD:LivraisOO/src/view/Window.java
 
 	private void setSize() {
 		setSize(1000, 800);
@@ -46,31 +35,13 @@ public class Window extends JFrame{
 		textualView.setSize(100, 400);
 		textualView.setLocation(380, 0);
 	}
-
-	private void showMessage(String message) {
-		messageBox.setText(message);
-	}
-	
-	public void drawMap(Plan map){
-=======
-	
-	private void setSize(){
-		setSize(1000, 800);
-		messageBox.setSize(480,60);
-		messageBox.setLocation(0,400);
-		graphicView.setLocation(0, 0);
-		graphicView.setSize(800, 740);
-		textualView.setSize(100,400);
-		textualView.setLocation(380,0);
-	}
 	
 	@SuppressWarnings("unused")
 	private void showMessage(String message){
 		messageBox.setText(message);
 	}
 	
-	public void drawMap(Map map){
->>>>>>> 9a535b2919dfa1f665df945f77774da1c8c10f49:Code java/LivraisOO/src/view/Window.java
+	public void drawMap(Plan map){
 		this.graphicView.paintMap(map);
 	}
 }
