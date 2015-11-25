113f173f5cf4fa1d4df06d952bbb91371c2f94b1
biavaz <mb_vaz@hotmail.com>
2015-11-25 08:28:09 +0100

    Adition of save and open functions in Controller - Default State

--- LivraisOO/src/controller/DefaultState.java
+++ LivraisOO/src/controller/DefaultState.java
@@ -7,31 +7,29 @@ import model.Node;
 import view.Window;
 
 public abstract class DefaultState implements State {
+	
+	//Definition of the default comportaments
 
-	public void loadMap(File mapFile) {
-	}
+	public void loadMap(File mapFile) {}
 
-	public void loadDeliveries(File requestFile) {
-	}
+	public void loadDeliveries(File requestFile) {}
 
-	public void computeDeliveries(Plan map, Node deliveryRound) {
-	}
+	public void computeDeliveries(Plan map, Node deliveryRound) {}
 
-	public void addDelivery(Node deliveryRound, Plan m) {
-	}
+	public void addDelivery(Node deliveryRound, Plan m) {}
 
-	public void modifyDelivery(Node d) {
-	}
+	public void modifyDelivery(Node d) {}
 
-	public void deleteDelivery(Node deliveryRound, Node d) {
-	}
+	public void deleteDelivery(Node deliveryRound, Node d) {}
 
-	public void modifyDelivery(Node deliveryRound, Node d1, Node d2) {
-	}
+	public void modifyDelivery(Node deliveryRound, Node d1, Node d2) {}
 
-	public void generateRoadmap() {
-	}
+	public void generateRoadmap() {}
 
-	public void rightClick(Window window, CommandsList cmdList) {
-	}
+	public void rightClick(Window window, CommandsList cmdList) {}
+	
+	public void save(Plan map, Window window){};
+	
+	public void open(Plan map, CommandsList cmdList, Window window){}
 }
+
