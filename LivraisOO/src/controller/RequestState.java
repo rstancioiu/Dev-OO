30d07b7632e2ad86712dc7a5269563be60e055b4
biavaz <mb_vaz@hotmail.com>
2015-11-25 10:58:18 +0100

    Request State initial implementation

--- LivraisOO/src/controller/RequestState.java
+++ LivraisOO/src/controller/RequestState.java
@@ -1,5 +1,12 @@
 package controller;
 
+import model.Map;
+import model.Node;
+
 public class RequestState extends DefaultState {
+	@Override
+	public void computeDeliveries(Map map, Node deliveryRound){
+		//TODO: calcul de la tournée
+	}
 
 }
